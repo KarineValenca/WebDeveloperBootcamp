06906cce8b87f157f3f271d32f272f9e1515d5bc
Karine Valença <valenca.karine@gmail.com>
2018-09-04 11:28:46 -0300

    added nested arrays and objects exercise

---
+++ javascript module/movies.js
@@ -0,0 +1,30 @@
+var myMovies = [
+	{
+		title: "Garota dinamarquesa",
+		rating: 4,
+		hasWatched: true
+	},
+	{
+		title: "Um sonho de liberdade",
+		rating: 5,
+		hasWatched: false
+	},
+	{
+		title: "Mad Max",
+		rating: 4.5, 
+		hasWatched: false
+	}
+];
+
+myMovies.forEach(function(movie){
+	var result = "You have "
+	if(movie.hasWatched == true){
+		result += "watched ";
+	} else {
+		result += "not seen ";
+	}
+	result += "\"" + movie.title + "\" - ";
+	result += movie.rating + " stars";
+
+	console.log(result);
+});
