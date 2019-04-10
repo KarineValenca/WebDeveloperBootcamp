2d3e875f0b349bc18865c430b8c486025d4ddfe0
Karine Valenca <valenca.karine@gmail.com>
2019-04-09 22:45:24 -0300

    finish todo project

--- Todo list/assets/js/todos.js
+++ Todo list/assets/js/todos.js
@@ -1,11 +1,23 @@
 // check off scpecific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$(".delete").click(function(event){
+$("ul").on("click", ".delete", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class='delete'><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
