364cff9d2ff9226152a554ceee9dced8ebcce4df
Karine Valenca <valenca.karine@gmail.com>
2019-08-19 22:25:59 -0300

    added show route and some refactoring

--- YelpCampProject/app.js
+++ YelpCampProject/app.js
@@ -3,22 +3,25 @@ const express  = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose   = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp")
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 //schema setup
 
+
 const campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 /*Campground.create({
 	name: "Granite", 
-	image: "https://c1.hiqcdn.com/customcdn/500x500/blog/sites/default/files/camping-te.jpg"
+	image: "https://c1.hiqcdn.com/customcdn/500x500/blog/sites/default/files/camping-te.jpg",
+	description: "Granite camp. Very beautiful!"
 }, function(err, campground){
 	if(err){
 		console.log(err);
@@ -28,31 +31,28 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 	}
 })*/
 
-/*var campgrounds =[
-		{name: "Salmon", image: "http://www.guntherpublications.com/core/wp-content/uploads/2018/01/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
-		{name: "Granite", image: "https://c1.hiqcdn.com/customcdn/500x500/blog/sites/default/files/camping-te.jpg"},
-		{name: "Mountain", image: "https://cdn.pixabay.com/photo/2017/08/06/02/32/camp-2587926_960_720.jpg"}
-	];*/
-
 app.get("/", function(req, res){
 	res.render("landing");
 })
 
+// INDEX
 app.get("/campgrounds", function(req, res){
 	//GET ALL CAMPGROUNDS FROM DB
 	Campground.find({}, function(err, campgrounds){
 		if(err) {
 			console.log(err);
 		}else{
-			res.render("campgrounds", {campgrounds: campgrounds});
+			res.render("index", {campgrounds: campgrounds});
 		}
 	})
 })
 
+// CREATE
 app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description: description}
 	//Create a new campground and save to DB
 	Campground.create(newCampground, function(err, campground){
 		if(err){
@@ -64,9 +64,25 @@ app.post("/campgrounds", function(req, res){
 	})
 })
 
+// NEW
 app.get("/campgrounds/new", function(req, res){
-	res.render("newCampground")
+	res.render("new")
 })
+
+// SHOW
+app.get("/campgrounds/:id", function(req, res){
+	//find campground with id
+	Campground.findById(req.params.id, function(err, campground){
+		if(err) {
+			console.log(err);
+		}else {
+			//render show page
+			res.render("show", {campground: campground});
+		}
+	});
+
+})
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, function() { 
   console.log('YelpCamp Server has started!!!'); 
