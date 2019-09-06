7220e1e5701755ae7d7beab61aaee52c36db1982
Karine Valenca <valenca.karine@gmail.com>
2019-09-05 22:56:10 -0300

    create model structure

--- YelpCampProject/app.js
+++ YelpCampProject/app.js
@@ -2,34 +2,13 @@ const express  = require("express"),
 	app        = express(),
 	bodyParser = require("body-parser"),
 	mongoose   = require("mongoose");
+	Campground = require("./models/campground")
+	seedDB = require("./seeds")
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-//schema setup
-
-
-const campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-/*Campground.create({
-	name: "Granite", 
-	image: "https://c1.hiqcdn.com/customcdn/500x500/blog/sites/default/files/camping-te.jpg",
-	description: "Granite camp. Very beautiful!"
-}, function(err, campground){
-	if(err){
-		console.log(err);
-	}else{
-		console.log("Newly created campground:");
-		console.log(campground);
-	}
-})*/
+seedDB();
 
 app.get("/", function(req, res){
 	res.render("landing");
@@ -72,10 +51,11 @@ app.get("/campgrounds/new", function(req, res){
 // SHOW
 app.get("/campgrounds/:id", function(req, res){
 	//find campground with id
-	Campground.findById(req.params.id, function(err, campground){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, campground){
 		if(err) {
 			console.log(err);
 		}else {
+			console.log(campground);
 			//render show page
 			res.render("show", {campground: campground});
 		}
