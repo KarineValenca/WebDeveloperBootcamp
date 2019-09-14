33450fe1576fa5132083247b7041d39dea8e6e25
Karine Valenca <valenca.karine@gmail.com>
2019-09-14 18:33:54 -0300

    added register and login page

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -11,6 +11,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app")
 var app = express()
 app.set('view engine', 'ejs')
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(require("express-session")({
 	secret: "Raj is the best cat in the world",
 	resave: false,
@@ -20,8 +21,13 @@ app.use(require("express-session")({
 app.use(passport.initialize())
 app.use(passport.session())
 
+passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+
+// ======================
+// ROUTES
+// ======================
 
 app.get("/", function(req, res){
 	res.render("home")
@@ -30,6 +36,38 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
 	res.render("secret")
 })
+
+// Auth rouste
+
+// show sign up form
+app.get("/register", function(req, res) {
+	res.render("register")
+})
+
+//handling user sign up
+app.post("/register", function(req, res){
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render('register')
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/secret")
+		})
+	})
+})
+
+// Login Routes
+app.get("/login", function(req, res){
+	res.render("login")
+})
+
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/secret",
+	failureRedirect: "/login"
+}), function(req, res){
+})
+
 // Tell Express to listen for requests (start server)
 app.listen(3000, function() { 
   console.log('YelpCamp Server has started!!!'); 
