8f2aa455e72391c267ac1f75fc161ff8d45d7142
Karine Valenca <valenca.karine@gmail.com>
2019-09-18 20:57:47 -0300

    update branch

--- YelpCampProject/routes/campgrounds.js
+++ YelpCampProject/routes/campgrounds.js
@@ -51,8 +51,27 @@ router.get("/:id", function(req, res){
 })
 
 // EDIT
+router.get("/:id/edit", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds")
+		}else {
+			res.render("campgrounds/edit", {campground: campground})
+		}
+	})
+})
 
 // UPDATE
+router.put("/:id", function(req, res){
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, campground){
+		if(err){
+			res.redirect("/campgrounds")
+		}else {
+			res.redirect("/campgrounds/" + req.params.id)
+		}
+	})
+})
 
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
