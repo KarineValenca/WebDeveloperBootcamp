296340aaa097cc12a61bc6ec64dab44f9d6fad64
Karine Valenca <valenca.karine@gmail.com>
2019-09-20 16:32:04 -0300

    update branch

--- YelpCampProject/routes/comments.js
+++ YelpCampProject/routes/comments.js
@@ -36,7 +36,7 @@ router.post("/", isLoggedIn, function(req, res){
 	})
 })
 
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, comment){
 		if(err){
 			res.redirect("back")
@@ -47,7 +47,7 @@ router.get("/:comment_id/edit", function(req, res){
 	
 })
 
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment){
 		if(err){
 			res.redirect("back")
@@ -57,7 +57,7 @@ router.put("/:comment_id", function(req, res){
 	})
 })
 
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
 	Comment.findByIdAndRemove(req.params.comment_id, function(err, comment){
 		if(err){
 			res.redirect("back")
@@ -73,6 +73,26 @@ function isLoggedIn(req, res, next){
 		return next()
 	}
 	res.redirect("/login")
+}
+
+function checkCommentOwnership(req, res, next) {
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, comment){
+			if(err) {
+				console.log(err);
+				res.redirect("back")
+			}else {
+				if(comment.author.id.equals(req.user._id)){
+					next()
+				}else {
+					res.redirect("back")	
+				}
+				
+			}
+		})
+	} else{
+		res.redirect("back")
+	}
 }
 
 module.exports = router
