1973655b87bb8595d811d8c0b671396b231e9074
Karine Valenca <valenca.karine@gmail.com>
2019-09-23 23:39:46 -0300

    addeds flash messages

--- YelpCampProject/app.js
+++ YelpCampProject/app.js
@@ -4,11 +4,12 @@
 	mongoose      = require("mongoose"),
 	passport      = require("passport"),
 	LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override")
+	methodOverride = require("method-override"),
 	Campground = require("./models/campground"),
 	Comment    = require("./models/comment"),
 	User = require("./models/user"),
-	seedDB = require("./seeds")
+	seedDB = require("./seeds"),
+	flash = require("connect-flash")
 
 var commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"))
+app.use(flash())
 //seedDB()
 
 // Passport configuration
@@ -36,6 +38,8 @@ passport.deserializeUser(User.deserializeUser())
 
 app.use(function(req, res, next){
 	res.locals.currentUser = req.user
+	res.locals.error = req.flash("error")
+	res.locals.success = req.flash("success")
 	next()
 })
 
